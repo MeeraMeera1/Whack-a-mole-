d5ee69153ebe9c7b332b14b95b229c4779f7bac1
MeeraMeera1 <gatesameera98@gmail.com>
2020-11-04 13:49:59 -0500

    Implemented random popping up moles

--- mole.js
+++ mole.js
@@ -1,23 +1,27 @@
 // Write your JS here.
 window.addEventListener("DOMContentLoaded", () => {
+  setTimeout(() => {
+    popUpRandomMole();
+  }, 0);
 
+  function popUpRandomMole() {
+    const moleHeads = document.getElementsByClassName("wgs__mole-head");
+    // for (let moleHead of moleHeads) {
+    let randomNumber = Math.floor(Math.random() * 8);
+    let peekingMole = moleHeads[randomNumber];
+    peekingMole.classList.remove("wgs__mole-head--hidden");
+    console.log(peekingMole);
 
-
-
-
-  setInterval(() => {
-    function popUpRandomMole () {
-      const moleHeads = document.getElementsByClassName(".wgs__mole-head");
-      for(let moleHead of moleHeads){
-        let randomNumber = Math.floor(Math.random() * 8);
-        moleHead[randomNumber].classList.toggle("wgs__mole-head--hidden");
-      }
-    }
-
-  }, 1000);
-
-  function hideMole(param){
-    
+    setTimeout(() => {
+      hideMole(peekingMole);
+    }, 1000);
+    // }
   }
 
+  function hideMole(element) {
+    element.classList.add("wgs__mole-head--hidden");
+    setTimeout(() => {
+      popUpRandomMole();
+    }, 1000);
+  }
 });
