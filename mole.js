ac33529e99f83cfbb1005bd1f3a1b56b6d0d2390
chris <percist@gmail.com>
2020-11-04 13:24:25 -0500

    start timing function

--- mole.js
+++ mole.js
@@ -1,9 +1,23 @@
 // Write your JS here.
 window.addEventListener("DOMContentLoaded", () => {
+
+
+
+
+
   setInterval(() => {
-    const moleHeads = document.querySelectorAll(".wgs__mole-head");
-    for (let moleHead of moleHeads) {
-      moleHead.classList.toggle("wgs__mole-head--hidden");
+    function popUpRandomMole () {
+      const moleHeads = document.getElementsByClassName(".wgs__mole-head");
+      for(let moleHead of moleHeads){
+        let randomNumber = Math.floor(Math.random() * 8);
+        moleHead[randomNumber].classList.toggle("wgs__mole-head--hidden");
+      }
     }
+
   }, 1000);
+
+  function hideMole(param){
+    
+  }
+
 });
